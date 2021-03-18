363937c2aae122ae0d4a1c61edb7c17e07ad5ebb
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2021-03-18 14:14:14 +0100

    More cleanup.

--- src_gap/Group.h
+++ src_gap/Group.h
@@ -32,19 +32,19 @@ public:
     UnbindCycles(S);
     size_tint = Order<Telt_inp,Tint_inp>(S);
   }
-  Group<Telt_inp,Tint_inp> Stabilizer_OnPoints(int const& x)
+  Group<Telt_inp,Tint_inp> Stabilizer_OnPoints(int const& x) const
   {
     return Group(Kernel_Stabilizer_OnPoints<Telt_inp,Tint_inp>(S, x));
   }
-  std::pair<bool,Telt_inp> RepresentativeAction_OnPoints(int const& x1, int const& x2)
+  std::pair<bool,Telt_inp> RepresentativeAction_OnPoints(int const& x1, int const& x2) const
   {
     return Kernel_RepresentativeAction_OnPoints<Telt_inp,Tint_inp>(S, x1, x2);
   }
-  Group<Telt_inp,Tint_inp> Stabilizer_OnSets(Face const& f)
+  Group<Telt_inp,Tint_inp> Stabilizer_OnSets(Face const& f) const
   {
     return Group(Kernel_Stabilizer_OnSets<Telt_inp,Tint_inp>(S, f));
   }
-  std::pair<bool,Telt_inp> RepresentativeAction_OnSets(Face const& f1, Face const& f2)
+  std::pair<bool,Telt_inp> RepresentativeAction_OnSets(Face const& f1, Face const& f2) const
   {
     return Kernel_RepresentativeAction_OnSets<Telt_inp,Tint_inp>(S, f1, f2);
   }
@@ -52,13 +52,17 @@ public:
   {
     return Kernel_GeneratorsOfGroup(S);
   }
-  Face CanonicalImage(Face const& f)
+  Face CanonicalImage(Face const& f) const
   {
     return Kernel_CanonicalImage<Telt_inp,Tint_inp>(S, f);
   }
   Tint_inp size() const
   {
     return size_tint;
+  }
+  int n_act() const
+  {
+    return S->comm->n;
   }
 private:
   StabChain<Telt_inp> S;
