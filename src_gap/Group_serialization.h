916e82bf67315dcab5f4270131725cda6e8a9984
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2021-07-06 13:54:35 +0200

    Resolution of compilation bugs.

--- src_gap/Group_serialization.h
+++ src_gap/Group_serialization.h
@@ -22,9 +22,8 @@
 
 namespace boost::serialization {
 
-  template<class Archive, typename Tgroup>
-  inline void load(Archive & ar, Tgroup & val, const unsigned int version) {
-    using Telt = typename Tgroup::Telt;
+  template<class Archive, typename Telt, typename Tint>
+  inline void load(Archive & ar, permutalib::Group<Telt,Tint> & val, const unsigned int version) {
     using Tidx = typename Telt::Tidx;
     std::cerr << "load(Tgroup), step 1\n";
     Tidx n_act;
@@ -40,16 +39,15 @@ namespace boost::serialization {
       Telt eGen(eList);
       LGen.push_back(eGen);
     }
-    val = Tgroup(LGen, n_act);
+    val = permutalib::Group<Telt,Tint>(LGen, n_act);
     std::cerr << "load(Tgroup), step 2\n";
   }
 
-  template<class Archive, typename Tgroup>
-  inline void save(Archive & ar, Tgroup const& val, const unsigned int version) {
-    using Telt = typename Tgroup::Telt;
+  template<class Archive, typename Telt, typename Tint>
+  inline void save(Archive & ar, permutalib::Group<Telt,Tint> const& val, const unsigned int version) {
     using Tidx = typename Telt::Tidx;
     std::cerr << "save(Tgroup), step 1\n";
-    Tidx n_act = val->n_act();
+    Tidx n_act = val.n_act();
     ar & make_nvp("n_act", n_act);
     std::vector<Telt> LGen = val.GeneratorsOfGroup();
     size_t n_gen=LGen.size();
@@ -57,15 +55,15 @@ namespace boost::serialization {
     for (size_t i_gen=0; i_gen<n_gen; i_gen++) {
       const Telt& eGen = LGen[i_gen];
       for (Tidx i=0; i<n_act; i++) {
-        Tidx val = eGen.at(i);
-        ar & make_nvp("val", val);
+        Tidx pnt = eGen.at(i);
+        ar & make_nvp("val", pnt);
       }
     }
     std::cerr << "save(Tgroup), step 2\n";
   }
 
-  template<class Archive, typename Tgroup>
-  inline void serialize(Archive & ar, Tgroup & val, const unsigned int version) {
+  template<class Archive, typename Telt, typename Tint>
+  inline void serialize(Archive & ar, permutalib::Group<Telt,Tint> & val, const unsigned int version) {
     std::cerr << "split_free(Tgroup), step 1\n";
     split_free(ar, val, version);
     std::cerr << "split_free(Tgroup), step 2\n";
