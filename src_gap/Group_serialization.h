700b8d844b51c2025610c2357be75e786856fd26
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2021-07-06 11:23:34 +0200

    Add more serialization test.

--- src_gap/Group_serialization.h
+++ src_gap/Group_serialization.h
@@ -1,6 +1,23 @@
 #ifndef INCLUDE_GROUP_SERIALIZATION_H
 #define INCLUDE_GROUP_SERIALIZATION_H
 
+// Boost serialization
+
+#include <boost/archive/tmpdir.hpp>
+
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+
+#include <boost/serialization/base_object.hpp>
+#include <boost/serialization/utility.hpp>
+#include <boost/serialization/list.hpp>
+#include <boost/serialization/assume_abstract.hpp>
+#include <boost/serialization/split_member.hpp>
+#include <boost/serialization/split_free.hpp>
+
+
 
 
 namespace boost::serialization {
---
+++ src_gap/TestSerialization.cpp
@@ -0,0 +1,80 @@
+#include "Permutation.h"
+#include "gmpxx.h"
+#include <fstream>
+
+#include "Group.h"
+#include "Group_serialization.h"
+
+
+int main(int argc, char *argv[])
+{
+  try {
+    using Tidx = uint16_t;
+    using Telt = permutalib::SingleSidedPerm<Tidx>;
+    using Tint = mpz_class;
+    using Tgroup = permutalib::Group<Telt,Tint>;
+    if (argc != 2) {
+      std::cerr << "TestSerialization [EXMP]\n";
+      throw PermutalibException{1};
+    }
+    std::string InputFile = argv[1];
+    //
+    std::ifstream is(InputFile);
+    int nGroup;
+    is >> nGroup;
+    for (int iGroup=0; iGroup<nGroup; iGroup++) {
+      //
+      // Reading one group
+      //
+      size_t nbGen;
+      Tidx n;
+      is >> nbGen;
+      is >> n;
+      std::cerr << "iGroup=" << iGroup << " n=" << n << " nbGen=" << nbGen << "\n";
+      std::vector<Telt> LGen(nbGen);
+      for (size_t iGen=0; iGen<nbGen; iGen++) {
+        std::vector<Tidx> ePermV(n);
+        for (Tidx i=0; i<n; i++) {
+          Tidx eVal;
+          is >> eVal;
+          ePermV[i]=eVal;
+        }
+        Telt ePerm(ePermV);
+        LGen[iGen] = ePerm;
+      }
+      Tgroup eG(LGen, n);
+      //
+      // Setting the archives
+      //
+      std::string filename = "/tmp/Group_boost_archive_" + std::to_string(iGroup);
+      // save data to archive
+      {
+        std::ofstream ofs(filename);
+        boost::archive::text_oarchive oa(ofs);
+        // write class instance to archive
+        oa << eG;
+    	// archive and stream closed when destructors are called
+      }
+      // load data from archive
+      Tgroup fG;
+      {
+        // create and open an archive for input
+        std::ifstream ifs(filename);
+        boost::archive::text_iarchive ia(ifs);
+        // read class state from archive
+        ia >> fG;
+        // archive and stream closed when destructors are called
+      }
+      bool test= eG == fG;
+      if (!test) {
+        std::cerr << "The serialization failed\n";
+        throw PermutalibException{1};
+      }
+    }
+    std::cerr << "Correct termination of the program\n";
+  }
+  catch (PermutalibException const& e) {
+    std::cerr << "Erroneous completion of the program\n";
+    exit(e.eVal);
+  }
+}
