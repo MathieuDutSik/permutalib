cc5fabda9e80565f8c1a7d4482b03daf258f91fa
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2022-04-17 20:08:33 +0200

    Generalize the test for getting the timings.

--- src_gap/TestEnumerationElement.cpp
+++ src_gap/TestEnumerationElement.cpp
@@ -10,14 +10,17 @@ int main(int argc, char *argv[]) {
     using Telt = permutalib::SingleSidedPerm<Tidx>;
     using Tint = mpz_class;
     using Tgroup = permutalib::Group<Telt, Tint>;
-    if (argc != 3) {
-      std::cerr << "TestEnumerationElement [EXMP] [limit]\n";
+    if (argc != 4) {
+      std::cerr << "TestEnumerationElement [EXMP] [limit] [case]\n";
       throw PermutalibException{1};
     }
     std::string InputFile = argv[1];
     int limit_i;
     (void)sscanf(argv[2], "%d", &limit_i);
     Tint limit = limit_i;
+    int ecase;
+    (void)sscanf(argv[3], "%d", &ecase);
+    std::cerr << "ecase=" << ecase << "\n";
     //
     std::ifstream is(InputFile);
     int nGroup;
@@ -33,7 +36,7 @@ int main(int argc, char *argv[]) {
                 << " nbGen=" << nbGen << "\n";
       std::vector<Telt> LGen(nbGen);
       for (size_t iGen = 0; iGen < nbGen; iGen++) {
-        std::cerr << "iGen=" << iGen << "/" << nbGen << " n=" << int(n) << "\n";
+        //        std::cerr << "iGen=" << iGen << "/" << nbGen << " n=" << int(n) << "\n";
         std::vector<Tidx> ePermV(n);
         for (Tidx i = 0; i < n; i++) {
           int eVal_i;
@@ -53,28 +56,41 @@ int main(int argc, char *argv[]) {
       Telt id(n);
       Tgroup eG(LGen, id);
       std::cerr << "|eG|=" << eG.size() << " limit=" << limit << "\n";
-      if (eG.size() <= limit || limit < 0) { // Some groups can be too large to
-                                             // iterate over their elements.
-        std::unordered_set<Telt> ListElt;
-        size_t n_iter = 0;
-        for (auto &elt : eG) {
-          ListElt.insert(elt);
-          if (Tint(ListElt.size()) > eG.size()) {
-            std::cerr << "We found more elements than the size of the group. "
-                         "Clear bug\n";
+      // Some groups can be too large to iterate over their elements.
+      if (eG.size() <= limit || limit < 0) {
+        if (ecase == 1) {
+          std::unordered_set<Telt> ListElt;
+          size_t n_iter = 0;
+          for (auto &elt : eG) {
+            ListElt.insert(elt);
+            if (Tint(ListElt.size()) > eG.size()) {
+              std::cerr << "We found more elements\n";
+              std::cerr << "than the group order\n";
+              throw PermutalibException{1};
+            }
+            n_iter++;
+          }
+          if (n_iter != ListElt.size()) {
+            std::cerr << "Some elements were found several times. Clear bug\n";
             throw PermutalibException{1};
           }
-          n_iter++;
+          if (Tint(n_iter) != eG.size()) {
+            std::cerr << "Some elements were missed. Clear bug\n";
+            throw PermutalibException{1};
+          }
         }
-        if (n_iter != ListElt.size()) {
-          std::cerr << "Some elements were found several times. Clear bug\n";
-          throw PermutalibException{1};
-        }
-        if (Tint(n_iter) != eG.size()) {
-          std::cerr << "Some elements were missed. Clear bug\n";
-          throw PermutalibException{1};
+        if (ecase == 2) {
+          std::chrono::time_point<std::chrono::system_clock> time1 = std::chrono::system_clock::now();
+          size_t n_iter = 0;
+          for (auto &elt : eG) {
+            n_iter++;
+          }
+          std::chrono::time_point<std::chrono::system_clock> time2 = std::chrono::system_clock::now();
+          std::cerr << "n_iter=" << n_iter << " seconds=" << std::chrono::duration_cast<std::chrono::seconds>(time2 - time1).count() << "\n";
         }
         n_treat++;
+      } else {
+        std::cerr << "    Not treated because it is too large\n";
       }
     }
     std::cerr << "n_treat = " << n_treat << "\n";
