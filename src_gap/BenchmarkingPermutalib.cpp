d40fe831ecce8f7007c4d6fa5693fffd848dfb32
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2023-03-27 09:36:19 +0200

    More debugging of the stuff.

--- src_gap/BenchmarkingPermutalib.cpp
+++ src_gap/BenchmarkingPermutalib.cpp
@@ -4,6 +4,155 @@
 #include "gmpxx.h"
 #include <fstream>
 
+
+template<typename Tgroup>
+void full_check(Tgroup const& eG, std::string const& opt, int64_t const& n_iter, size_t & siz_control) {
+  using Telt = typename Tgroup::Telt;
+  using Tidx = typename Telt::Tidx;
+  Tidx n = eG.n_act();
+  auto random_face = [](const Tidx &len) -> permutalib::Face {
+    permutalib::Face eFace(len);
+    for (Tidx i = 0; i < len; i++) {
+      int eVal = Tidx(random()) % 2;
+      eFace[i] = eVal;
+    }
+    return eFace;
+  };
+  auto bench_canonical = [&]() -> void {
+    for (int64_t iter = 0; iter < n_iter; iter++) {
+      permutalib::Face eFace = random_face(n);
+      permutalib::Face set_can = eG.CanonicalImage(eFace);
+      siz_control += set_can.count();
+    }
+  };
+  auto bench_stabilizer = [&]() -> void {
+    for (int64_t iter = 0; iter < n_iter; iter++) {
+      permutalib::Face eFace = random_face(n);
+      Tgroup eG2 = eG.Stabilizer_OnSets(eFace);
+      siz_control += eFace.count();
+    }
+  };
+  auto bench_pointstabilizer = [&]() -> void {
+    for (int64_t iter = 0; iter < n_iter; iter++) {
+      Tidx pos = Tidx(random()) % n;
+      Tgroup eG2 = eG.Stabilizer_OnPoints(pos);
+      siz_control += size_t(pos);
+    }
+  };
+  auto bench_pointrepresentative = [&]() -> void {
+    for (int64_t iter = 0; iter < n_iter; iter++) {
+      Tidx pos1 = Tidx(random()) % n;
+      Tidx pos2 = Tidx(random()) % n;
+      std::optional<Telt> eP = eG.RepresentativeAction_OnPoints(pos1, pos2);
+      siz_control += size_t(pos1) + size_t(pos2);
+      if (eP)
+        siz_control++;
+    }
+  };
+  auto check_canonical = [&]() -> void {
+    for (int64_t iter = 0; iter < n_iter; iter++) {
+      permutalib::Face eFace1 = random_face(n);
+      permutalib::Face set_can1 = eG.CanonicalImage(eFace1);
+      for (int i = 0; i < 4; i++) {
+        Telt u = eG.random();
+        permutalib::Face eFace2 = OnSets(eFace1, u);
+        permutalib::Face set_can2 = eG.CanonicalImage(eFace2);
+        if (set_can1 != set_can2) {
+          std::cerr << "Canonicalization failed\n";
+          throw permutalib::PermutalibException{1};
+        }
+        std::pair<permutalib::Face,Tgroup> pairCan = eG.PairCanonicalImageStabilizer(eFace2);
+        if (pairCan.first != set_can2) {
+          std::cerr << "We fail at that obvious step\n";
+          throw permutalib::PermutalibException{1};
+        }
+        Tgroup eStab = eG.Stabilizer_OnSets(pairCan.first);
+        if (!eStab.IsSubgroup(pairCan.second)) {
+          std::cerr << "We fail that pairCan.second is not a subgroup of the stabilizer\n";
+          std::cerr << "|eStab|=" << eStab.size() << " |pairCan.second|=" << pairCan.second.size() << "\n";
+          throw permutalib::PermutalibException{1};
+        }
+      }
+      siz_control += eFace1.count();
+    }
+  };
+  auto check_exhaustive_canonical = [&]() -> void {
+    for (int64_t iter = 0; iter < n_iter; iter++) {
+      permutalib::Face eFace1 = random_face(n);
+      permutalib::Face set_can1 = eG.ExhaustiveCanonicalImage(eFace1);
+      for (int i = 0; i < 4; i++) {
+        Telt u = eG.random();
+        permutalib::Face eFace2 = OnSets(eFace1, u);
+        permutalib::Face set_can2 = eG.ExhaustiveCanonicalImage(eFace2);
+        if (set_can1 != set_can2) {
+          std::cerr << "ExhaustiveCanonicalization failed\n";
+          throw permutalib::PermutalibException{1};
+        }
+      }
+      siz_control += eFace1.count();
+    }
+  };
+  auto check_representative = [&]() -> void {
+    for (int64_t iter = 0; iter < n_iter; iter++) {
+      permutalib::Face eFace1 = random_face(n);
+      Telt u = eG.random();
+      permutalib::Face eFace2 = OnSets(eFace1, u);
+      std::optional<Telt> test =
+        eG.RepresentativeAction_OnSets(eFace1, eFace2);
+      if (!test) {
+        std::cerr << "RepresentativeAction_OnSets error\n";
+        throw permutalib::PermutalibException{1};
+      }
+      siz_control += eFace1.count();
+    }
+  };
+  auto check_stabilizer = [&]() -> void {
+    for (int64_t iter = 0; iter < n_iter; iter++) {
+      permutalib::Face eFace1 = random_face(n);
+      Tgroup eG1 = eG.Stabilizer_OnSets(eFace1);
+      for (int i = 0; i < 4; i++) {
+        Telt u = eG.random();
+        permutalib::Face eFace2 = OnSets(eFace1, u);
+        Tgroup eG2 = eG.Stabilizer_OnSets(eFace2);
+        if (eG1.size() != eG2.size()) {
+          std::cerr << "Stabilizer_OnSets error\n";
+          throw permutalib::PermutalibException{1};
+        }
+      }
+      siz_control += eFace1.count();
+    }
+  };
+  //
+  if (opt == "canonical")
+    bench_canonical();
+  if (opt == "stabilizer")
+    bench_stabilizer();
+  if (opt == "pointstabilizer")
+    bench_pointstabilizer();
+  if (opt == "pointrepresentative")
+    bench_pointrepresentative();
+  if (opt == "check_canonical")
+    check_canonical();
+  if (opt == "check_exhaustive_canonical")
+    check_exhaustive_canonical();
+  if (opt == "check_representative")
+    check_representative();
+  if (opt == "check_stabilizer")
+    check_stabilizer();
+  //
+  if (opt == "all") {
+    bench_canonical();
+    bench_stabilizer();
+    bench_pointstabilizer();
+    bench_pointrepresentative();
+    check_canonical();
+    check_exhaustive_canonical();
+    check_representative();
+    check_stabilizer();
+  }
+}
+
+
 int main(int argc, char *argv[]) {
   try {
     //    using Tidx = int16_t;
@@ -13,36 +162,49 @@ int main(int argc, char *argv[]) {
     using Tint = mpz_class;
     using Tgroup = permutalib::Group<Telt, Tint>;
     int64_t n_iter = 50;
-    if (argc != 3 && argc != 4) {
-      std::cerr << "BenchmarkingPermutalib [EXMP] [opt]\n";
+    std::vector<std::string> ListOpts = {
+        "canonical",        "stabilizer",
+        "pointstabilizer",  "pointrepresentative",
+        "check_canonical",   "check_exhaustive_canonical",
+        "check_representative",
+        "check_stabilizer", "all"};
+    if (argc != 4 && argc != 5) {
+      std::cerr << "BenchmarkingPermutalib [InputFile] [limit] [opt]\n";
       std::cerr << "or\n";
-      std::cerr << "BenchmarkingPermutalib [EXMP] [opt] [n_iter]\n";
+      std::cerr << "BenchmarkingPermutalib [InputFile] [limit] [opt] [n_iter]\n";
+      std::cerr << "\n";
+      std::cerr << "    ----- possible options ----\n";
+      std::cerr << "\n";
+      std::cerr << "InputFile: The list of groups as an input file\n";
+      std::cerr << "limit: the limit in group size. E.g. 1000 or -1 for no limit\n";
+      std::cerr << "opt: The list of allowed options:\n";
+      for (auto &opt : ListOpts)
+        std::cerr << " " << opt;
+      std::cerr << "\n";
+      std::cerr << "n_iter: the number of random iteration done. if absent then 50\n";
       throw permutalib::PermutalibException{1};
     }
     std::string InputFile = argv[1];
-    std::string opt = argv[2];
-    if (argc == 4) {
-      n_iter = permutalib::ReadScalar<int64_t>(argv[3]);
+    std::cerr << "InputFile=" << InputFile << "\n";
+    std::string limit_s = argv[2];
+    int limit = permutalib::ReadScalar<int64_t>(limit_s);
+    std::cerr << "limit=" << limit << "\n";
+    std::string opt = argv[3];
+    std::cerr << "opt=" << opt << "\n";
+    if (argc == 5) {
+      n_iter = permutalib::ReadScalar<int64_t>(argv[4]);
       std::cerr << "Using input value n_iter=" << n_iter << "\n";
     } else {
       std::cerr << "Using default value of 50 on n_iter\n";
     }
-    std::vector<std::string> ListOpts = {
-        "canonical",        "stabilizer",
-        "pointstabilizer",  "pointrepresentative",
-        "check_canonical",  "check_representative",
-        "check_stabilizer", "all"};
+    std::cerr << "n_iter=" << n_iter << "\n";
     bool IsMatch = false;
     for (auto &e_opt : ListOpts) {
       if (e_opt == opt)
         IsMatch = true;
     }
     if (!IsMatch) {
-      std::cerr << "opt=" << opt << "\n";
-      std::cerr << "ListOpts =";
-      for (auto &e_opt : ListOpts)
-        std::cerr << " " << e_opt;
-      std::cerr << "\n";
+      std::cerr << "opt=" << opt << " does not match\n";
       std::cerr << "Please select an option that is allowed\n";
       throw permutalib::PermutalibException{1};
     }
@@ -51,6 +213,7 @@ int main(int argc, char *argv[]) {
     size_t siz_control = 0;
     int nGroup;
     is >> nGroup;
+    int n_group_treated = 0;
     for (int iGroup = 0; iGroup < nGroup; iGroup++) {
       //      std::cerr << "iGroup=" << iGroup << " / " << nGroup << "\n";
       size_t nbGen;
@@ -83,132 +246,16 @@ int main(int argc, char *argv[]) {
       Tgroup eG(LGen, id);
       std::cerr << "  |eG|=" << eG.size() << "\n";
       //
-      auto random_face = [](const Tidx &len) -> permutalib::Face {
-        permutalib::Face eFace(len);
-        for (Tidx i = 0; i < len; i++) {
-          int eVal = Tidx(random()) % 2;
-          eFace[i] = eVal;
-        }
-        return eFace;
-      };
-      auto bench_canonical = [&]() -> void {
-        for (int64_t iter = 0; iter < n_iter; iter++) {
-          permutalib::Face eFace = random_face(n);
-          permutalib::Face set_can = eG.CanonicalImage(eFace);
-          siz_control += set_can.count();
-        }
-      };
-      auto bench_stabilizer = [&]() -> void {
-        for (int64_t iter = 0; iter < n_iter; iter++) {
-          permutalib::Face eFace = random_face(n);
-          Tgroup eG2 = eG.Stabilizer_OnSets(eFace);
-          siz_control += eFace.count();
-        }
-      };
-      auto bench_pointstabilizer = [&]() -> void {
-        for (int64_t iter = 0; iter < n_iter; iter++) {
-          Tidx pos = Tidx(random()) % n;
-          Tgroup eG2 = eG.Stabilizer_OnPoints(pos);
-          siz_control += size_t(pos);
-        }
-      };
-      auto bench_pointrepresentative = [&]() -> void {
-        for (int64_t iter = 0; iter < n_iter; iter++) {
-          Tidx pos1 = Tidx(random()) % n;
-          Tidx pos2 = Tidx(random()) % n;
-          std::optional<Telt> eP = eG.RepresentativeAction_OnPoints(pos1, pos2);
-          siz_control += size_t(pos1) + size_t(pos2);
-          if (eP)
-            siz_control++;
-        }
-      };
-      auto check_canonical = [&]() -> void {
-        for (int64_t iter = 0; iter < n_iter; iter++) {
-          permutalib::Face eFace1 = random_face(n);
-          permutalib::Face set_can1 = eG.CanonicalImage(eFace1);
-          for (int i = 0; i < 4; i++) {
-            Telt u = eG.random();
-            permutalib::Face eFace2 = OnSets(eFace1, u);
-            permutalib::Face set_can2 = eG.CanonicalImage(eFace2);
-            if (set_can1 != set_can2) {
-              std::cerr << "Canonicalization failed\n";
-              throw permutalib::PermutalibException{1};
-            }
-            std::pair<permutalib::Face,Tgroup> pairCan = eG.PairCanonicalImageStabilizer(eFace2);
-            if (pairCan.first != set_can2) {
-              std::cerr << "We fail at that obvious step\n";
-              throw permutalib::PermutalibException{1};
-            }
-            Tgroup eStab = eG.Stabilizer_OnSets(pairCan.first);
-            if (!eStab.IsSubgroup(pairCan.second)) {
-              std::cerr << "We fail that pairCan.second is not a subgroup of the stabilizer\n";
-              std::cerr << "|eStab|=" << eStab.size() << " |pairCan.second|=" << pairCan.second.size() << "\n";
-              throw permutalib::PermutalibException{1};
-            }
-          }
-          siz_control += eFace1.count();
-        }
-      };
-      auto check_representative = [&]() -> void {
-        for (int64_t iter = 0; iter < n_iter; iter++) {
-          permutalib::Face eFace1 = random_face(n);
-          Telt u = eG.random();
-          permutalib::Face eFace2 = OnSets(eFace1, u);
-          std::optional<Telt> test =
-              eG.RepresentativeAction_OnSets(eFace1, eFace2);
-          if (!test) {
-            std::cerr << "RepresentativeAction_OnSets error\n";
-            throw permutalib::PermutalibException{1};
-          }
-          siz_control += eFace1.count();
-        }
-      };
-      auto check_stabilizer = [&]() -> void {
-        for (int64_t iter = 0; iter < n_iter; iter++) {
-          permutalib::Face eFace1 = random_face(n);
-          Tgroup eG1 = eG.Stabilizer_OnSets(eFace1);
-          for (int i = 0; i < 4; i++) {
-            Telt u = eG.random();
-            permutalib::Face eFace2 = OnSets(eFace1, u);
-            Tgroup eG2 = eG.Stabilizer_OnSets(eFace2);
-            if (eG1.size() != eG2.size()) {
-              std::cerr << "Stabilizer_OnSets error\n";
-              throw permutalib::PermutalibException{1};
-            }
-          }
-          siz_control += eFace1.count();
-        }
-      };
-      //
-      if (opt == "canonical")
-        bench_canonical();
-      if (opt == "stabilizer")
-        bench_stabilizer();
-      if (opt == "pointstabilizer")
-        bench_pointstabilizer();
-      if (opt == "pointrepresentative")
-        bench_pointrepresentative();
-      if (opt == "check_canonical")
-        check_canonical();
-      if (opt == "check_representative")
-        check_representative();
-      if (opt == "check_stabilizer")
-        check_stabilizer();
-      //
-      if (opt == "all") {
-        bench_canonical();
-        bench_stabilizer();
-        bench_pointstabilizer();
-        bench_pointrepresentative();
-        check_canonical();
-        check_representative();
-        check_stabilizer();
+      if (eG.size() < limit || limit < 0) {
+        full_check(eG, opt, n_iter, siz_control);
+        n_group_treated++;
       }
     }
     //
     std::cerr << "Control values (should stay the same because we do "
                  "deterministic random sets)="
               << siz_control << "\n";
+    std::cerr << "Number of groups treated = " << n_group_treated << "\n";
     std::cerr << "CPP Normal completion of the program\n";
   } catch (permutalib::PermutalibException const &e) {
     std::cerr << "Erroneous completion of the program\n";
