cadc9a0a258bf53324a1077ef9988e23bc6f8b13
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-12-23 12:58:08 +0100

    Add the test program.

---
+++ src/GapComputeDoubleCosets.cpp
@@ -0,0 +1,48 @@
+// Copyright (C) 2022 Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
+
+// clang-format off
+#include "Permutation.h"
+#include "gmpxx.h"
+#include <fstream>
+#include "Group.h"
+// clang-format on
+
+int main(int argc, char *argv[]) {
+  try {
+    using Tidx = uint16_t;
+    using Telt = permutalib::SingleSidedPerm<Tidx>;
+    using Tint = mpz_class;
+    using Tgroup = permutalib::Group<Telt, Tint>;
+    using DoubleCosetComputer = typename Tgroup::DoubleCosetComputer;
+    if (argc != 2 && argc != 3) {
+      std::cerr << "GapAscendingChainPair [H_G]\n";
+      std::cerr << "or\n";
+      std::cerr << "GapAscendingChainPair [H_G] [OUT]\n";
+      throw permutalib::PermutalibException{1};
+    }
+    std::string File_HG = argv[1];
+    //
+    std::ifstream is(File_HG);
+    if (!is.good()) {
+      std::cerr << "is stream is invalid, not possible to read H_G\n";
+      throw permutalib::PermutalibException{1};
+    }
+    Tgroup eG = permutalib::ReadGroupFromStream<Tgroup>(is);
+    Tgroup eU = permutalib::ReadGroupFromStream<Tgroup>(is);
+    Tgroup eV = permutalib::ReadGroupFromStream<Tgroup>(is);
+    Tint size_G = eG.size();
+    Tint size_U = eU.size();
+    Tint size_V = eV.size();
+    std::cerr << "size_G=" << size_G << " size_U=" << size_U << " size_V=" << size_V << "\n";
+    //
+    DoubleCosetComputer dcc = eG.double_coset_computer(eU);
+    std::vector<Telt> list_dcc = dcc.double_cosets(eV);
+    KernelCheckDoubleCosets(eG.stab_chain(), eU.stab_chain(), eV.stab_chain(), list_dcc);
+    //
+    std::cerr << "CPP Normal completion of the program\n";
+  } catch (permutalib::PermutalibException const &e) {
+    std::cerr << "Erroneous completion of the program\n";
+    exit(e.eVal);
+  }
+  return 0;
+}
